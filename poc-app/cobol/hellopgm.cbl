f19114b1bc6be0e806a12b49bfa2723ae6bd720f
PROY <PROY@TVT7111.svl.ibm.com>
2022-09-16 06:24:21 -0400

    fix issues

--- poc-app/cobol/hellopgm.cbl
+++ poc-app/cobol/hellopgm.cbl
@@ -6,11 +6,7 @@
        OBJECT-COMPUTER. IBM-FLEX-ES.
        DATA DIVISION.
        PROCEDURE DIVISION.
-<<<<<<< HEAD
            DISPLAY 'HELLO WORLD'.
            DISPLAY 'Test for Impact Build'
-=======
-           DISPLAY 'HELLO'.
->>>>>>> cdd8775e2cbaec1fd36525fd384de26c1db3941b
            STOP RUN.
       *
